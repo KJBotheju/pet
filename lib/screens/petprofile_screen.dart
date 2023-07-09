de367fea4bb20590a24a8e8b8d8a6564a50be64b
Kavinda Botheju <117912732+KJBotheju@users.noreply.github.com>
2023-07-09 21:30:30 +0530

    update pet profile

--- lib/screens/petprofile_screen.dart
+++ lib/screens/petprofile_screen.dart
@@ -1,7 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'dart:io';
 import 'package:pet/widgets/constant.dart';
+
+class Pet {
+  String name;
+  String image;
+  int age;
+
+  Pet({required this.name, required this.image, required this.age});
+}
 
 class PetProfilePage extends StatefulWidget {
   @override
@@ -11,170 +21,256 @@ class PetProfilePage extends StatefulWidget {
 class _PetProfilePageState extends State<PetProfilePage> {
   List<Pet> pets = [];
 
+  TextEditingController nameController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+
+  File? _image;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        foregroundColor: Colors.black,
         backgroundColor: kPrimaryColor,
-        title: Text('Pet Profiles'),
+        foregroundColor: Colors.black,
+        title: Text('Pet Profile'),
       ),
       body: ListView.builder(
         itemCount: pets.length,
         itemBuilder: (context, index) {
-          return _buildPetProfile(pets[index]);
+          Pet pet = pets[index];
+          return Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Card(
+              elevation: 4.0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 16.0),
+                  CircleAvatar(
+                    radius: 80.0,
+                    backgroundImage: FileImage(File(pet.image)),
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Name: ${pet.name}',
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'Age: ${pet.age}',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  SizedBox(height: 24.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          _updatePetDetails(pet, index);
+                        },
+                        child: Text('Update'),
+                      ),
+                      SizedBox(width: 16.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          _deletePet(index);
+                        },
+                        child: Text('Delete'),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16.0),
+                ],
+              ),
+            ),
+          );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _addNewPet();
-        },
         child: Icon(Icons.add),
+        onPressed: () {
+          _addPetDetails();
+        },
       ),
     );
   }
 
-  Widget _buildPetProfile(Pet pet) {
-    return Card(
-      margin: EdgeInsets.all(10.0),
-      child: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              radius: 80.0,
-              backgroundImage: FileImage(File(pet.petImage)),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              'Pet Name: ${pet.petName}',
-              style: TextStyle(fontSize: 18.0),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'Pet Age: ${pet.petAge}',
-              style: TextStyle(fontSize: 18.0),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                _updatePet(pet);
-              },
-              child: Text('Update Pet Details'),
-            ),
-            SizedBox(height: 10.0),
-            ElevatedButton(
-              onPressed: () {
-                _deletePet(pet);
-              },
-              child: Text('Delete Pet'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  void _addPetDetails() {
+    nameController.text = '';
+    ageController.text = '';
+    _image = null;
 
-  void _addNewPet() {
-    setState(() {
-      Pet newPet = Pet(
-        petName: 'New Pet',
-        petAge: '0',
-        petImage: '', // Empty string initially
-      );
-      pets.add(newPet);
-    });
-  }
-
-  void _updatePet(Pet pet) {
-    showModalBottomSheet(
+    showDialog(
       context: context,
-      builder: (BuildContext context) {
-        String updatedPetName = pet.petName;
-        String updatedPetAge = pet.petAge;
-        String updatedPetImage = pet.petImage;
-
-        Future<void> _pickImage(ImageSource source) async {
-          final pickedImage = await ImagePicker().getImage(source: source);
-
-          if (pickedImage != null) {
-            setState(() {
-              updatedPetImage = pickedImage.path;
-            });
-          }
-        }
-
-        return Container(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Add Pet Details'),
+          content: Column(
             mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 80.0,
-                backgroundImage: updatedPetImage.isNotEmpty
-                    ? FileImage(File(updatedPetImage))
-                    : null,
-              ),
-              SizedBox(height: 10.0),
-              ElevatedButton(
-                onPressed: () {
-                  _pickImage(ImageSource.gallery);
-                },
-                child: Text('Choose Image'),
-              ),
-              SizedBox(height: 10.0),
+            children: [
+              _buildImagePicker(),
               TextField(
-                onChanged: (value) {
-                  updatedPetName = value;
-                },
+                controller: nameController,
                 decoration: InputDecoration(
-                  labelText: 'Pet Name',
+                  labelText: 'Name',
                 ),
               ),
-              SizedBox(height: 10.0),
               TextField(
-                onChanged: (value) {
-                  updatedPetAge = value;
-                },
+                controller: ageController,
+                keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                  labelText: 'Pet Age',
+                  labelText: 'Age',
                 ),
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    pet.petName = updatedPetName;
-                    pet.petAge = updatedPetAge;
-                    pet.petImage = updatedPetImage;
-                  });
-                  Navigator.pop(context);
-                },
-                child: Text('Save Changes'),
               ),
             ],
           ),
+          actions: [
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: Text('Save'),
+              onPressed: () {
+                setState(() {
+                  pets.add(
+                    Pet(
+                      name: nameController.text,
+                      image: _image != null ? _image!.path : '',
+                      age: int.parse(ageController.text),
+                    ),
+                  );
+                });
+                Navigator.pop(context);
+              },
+            ),
+          ],
         );
       },
     );
   }
 
-  void _deletePet(Pet pet) {
+  void _updatePetDetails(Pet pet, int index) {
+    TextEditingController nameEditController =
+        TextEditingController(text: pet.name);
+    TextEditingController ageEditController =
+        TextEditingController(text: pet.age.toString());
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Update Pet Details'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _buildImagePicker(),
+              TextField(
+                controller: nameEditController,
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                ),
+              ),
+              TextField(
+                controller: ageEditController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Age',
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: Text('Update'),
+              onPressed: () {
+                setState(() {
+                  pet.name = nameEditController.text;
+                  pet.age = int.parse(ageEditController.text);
+                  pet.image = _image != null ? _image!.path : pet.image;
+                });
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deletePet(int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Delete Pet'),
+          content: Text('Are you sure you want to delete this pet?'),
+          actions: [
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: Text('Delete'),
+              onPressed: () {
+                setState(() {
+                  pets.removeAt(index);
+                });
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildImagePicker() {
+    return Column(
+      children: [
+        _image != null
+            ? CircleAvatar(
+                backgroundImage: FileImage(_image!),
+                radius: 50,
+              )
+            : Placeholder(
+                fallbackHeight: 1,
+                fallbackWidth: 1,
+              ),
+        ElevatedButton(
+          onPressed: () {
+            _pickImage();
+          },
+          child: Text('Pick an Image'),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedImage = await picker.getImage(source: ImageSource.gallery);
+
     setState(() {
-      pets.remove(pet);
+      if (pickedImage != null) {
+        _image = File(pickedImage.path);
+      }
     });
   }
-}
-
-class Pet {
-  String petName;
-  String petAge;
-  String petImage;
-
-  Pet({
-    required this.petName,
-    required this.petAge,
-    required this.petImage,
-  });
 }
